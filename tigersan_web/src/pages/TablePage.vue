84ea19dae3491cef5b784e852a966c2a6e6b0f24
TigerSan <2898980478@qq.com>
2026-01-27 23:22:56 +0800

    normal commit

--- tigersan_web/src/pages/TablePage.vue
+++ tigersan_web/src/pages/TablePage.vue
@@ -1,19 +1,29 @@
 <template>
     <div class="table-page">
-        <div class="button-panel">
+        <div class="button-panel" :style="styleObject">
             <button class="refresh" @click="Save">Refresh</button>
             <button class="add" @click="Save">Add</button>
             <button class="delete" @click="Save">Delete</button>
             <button class="edit" @click="Save">Edit</button>
             <button class="save" @click="Save">Save</button>
         </div>
-        <Table :model="testTableModel"></Table>
+        <div class="table-panel" :style="styleObject">
+            <Table :model="testTableModel"></Table>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { Table } from '@/0_tigersan_ui/components'
+import navBarModel from '@/navBarModel';
 import { testTableModel } from '@/testTableModel'
+import { computed } from 'vue';
+
+let styleObject = computed(() => {
+    return {
+        width: `calc(100vw - ${navBarModel.IsOpen.value ? navBarModel.Width.value : 0}px)`
+    }
+})
 
 function Save() {
     console.log(testTableModel.RowDatas[0])
@@ -27,6 +37,7 @@ function Save() {
     .button-panel {
         display: flex;
         padding: 5px;
+        overflow: auto;
 
         * {
             margin-right: 5px;
@@ -51,6 +62,10 @@ function Save() {
         .save {
             background: var(--color-brand);
         }
+    }
+
+    .table-panel {
+        overflow: auto;
     }
 }
 </style>
